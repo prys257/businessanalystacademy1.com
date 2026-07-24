b8f354bb6db7eb5e0a1b64d4c51e077097ffc1fd
prys257 <themonarch257@gmail.com>
2026-07-24 21:32:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# businessanalystacademy.com
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,6 @@
+/** Tailwind is available for future mission components and utility-driven UI. */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: { extend: {} },
+  plugins: []
+};
